67a1b8f90a538fa00a4f64a3ee314de7855a32a2
Abdulc123 <149743322+Abdulc123@users.noreply.github.com>
2025-05-09 22:17:35 -0400

    External Camera connected to output

--- camera/capture.py
+++ camera/capture.py
@@ -8,7 +8,7 @@ class CameraCapture:
         :param width: Width of the video frame
         :parm height: Height of the video frame
         """
-        self.cap = cv2.VideoCapture(camera_index)
+        self.cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -17,7 +17,7 @@ class CameraCapture:
         
     def get_frame(self):
         """
-        Capture a single frame from the camera.
+        Capture a single frame from the camera each time its called
         :return: The captured frame or None if it failed
         """
         result, frame = self.cap.read()
@@ -27,8 +27,27 @@ class CameraCapture:
     
     def release(self):
         """
-        Release the camera resource
+        Frees up the webcame connection cv2 made when done with window
         """
         self.cap.release()
 
-    
+    def show_live_feed(self, window_name = "Pool Vision Live Camera Feed"):
+        """
+        (Optional) Show a lime camera feed window for debugging.
+        Press q to quit
+        """
+        while True:
+            frame = self.get_frame()
+            if frame is None:
+                print("CameraCapture: Failed to capture frame.")
+                break
+
+            cv2.imshow(window_name, frame)
+
+            key = cv2.waitKey(1)
+            # Closes window when q is pressed or close is selected
+            if key == ord('q') or cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
+                break
+
+        self.release()
+        cv2.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,6 @@
+from camera.capture import CameraCapture
+
+
+if __name__ == "__main__":
+    camera = CameraCapture()
+    camera.show_live_feed()
